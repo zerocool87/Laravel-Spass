8ad0016b44382080521c0fbf22872c9ff8047269
zero <zero@desktop-cqfrv78-1.home>
2026-01-26 23:18:50 +0100

    Add Laravel French framework translations (auth, pagination, passwords, validation)

---
+++ resources/lang/fr/passwords.php
@@ -0,0 +1,9 @@
+<?php
+
+return [
+    'password' => 'Les mots de passe doivent contenir au minimum six caractères et correspondre à la confirmation.',
+    'reset'    => 'Votre mot de passe a été réinitialisé !',
+    'sent'     => 'Nous vous avons envoyé par e-mail le lien de réinitialisation.',
+    'token'    => "Ce jeton de réinitialisation de mot de passe n'est pas valide.",
+    'user'     => "Nous ne trouvons pas d'utilisateur avec cette adresse e-mail.",
+];
