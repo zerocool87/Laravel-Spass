35efe3e2a53a608de338c829d5e58a34d935ebb4
zerocool87 <mat87000@gmail.com>
2026-01-30 14:47:50 +0100

    i18n: replace 'dashboard' → 'Tableau de bord', 'library' → 'Bibliothèque'

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -27,9 +27,7 @@
                         <a
                             href="{{ url('/dashboard') }}"
                             class="inline-block px-5 py-1.5 dark:text-[#EDEDEC] border-[#19140035] hover:border-[#1915014a] border text-[#1b1b18] dark:border-[#3E3E3A] dark:hover:border-[#62605b] rounded-sm text-sm leading-normal"
-                        >
-                            Dashboard
-                        </a>
+                        >{{ __('Dashboard') }}</a>
                     @else
                         <a
                             href="{{ route('login') }}"
