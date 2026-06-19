921da6641879ec9cc75574b943131e7dc004c0fe
Mathieu CAUZZI <mat87000@gmail.com>
2026-06-19 07:30:44 +0200

    Configure dev server to listen on all network interfaces

--- vite.config.js
+++ vite.config.js
@@ -9,10 +9,10 @@ export default defineConfig({
         }),
     ],
     server: {
-        host: '127.0.0.1',
+        host: '0.0.0.0',
         port: 5173,
         hmr: {
-            host: '127.0.0.1',
+            host: '192.168.1.14',
         },
         cors: true,
         headers: {
